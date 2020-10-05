55fc44399bce35dc731422f71bc0e8fbca500a3d
zhengwu <zhengwu@tang.cn>
2020-10-05 17:34:03 +0800

    完成了个人中心功能的开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,53 @@
+$(function () {
+    var form = layui.form
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1~6 个字符之间'
+            }
+        }
+    })
+    initUserInfo()
+    //初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.msg('获取用户信息失败!')
+                }
+                console.log(res);
+                //调用 form.val() 快速为表单赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+
+    //重置表单的数据
+    $('#btnReset').on('click', function (e) {
+        //阻止表单默认的重置行为
+        e.preventDefault()
+        initUserInfo()
+    })
+
+    //监听表单的提交事件
+    $('.layui-form').on('submit', function (e) {
+        //阻止表单默认提交行为
+        e.preventDefault()
+        //发起ajax数据请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.msg('更新用户信息失败!')
+                }
+                layui.msg('更新用户信息成功!')
+                //调用父页面中的方法,重新渲染用户的头像和信息
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
